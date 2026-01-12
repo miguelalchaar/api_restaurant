5a0b88a93617ae8c2024d04981705f8a5e170acc
miguelalchaar <alchaarcs@gmail.com>
2026-01-12 16:24:31 -0300

    feat: implement create product endpoint with validation

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,10 +1,25 @@
-import { AppError } from '@/utils/AppError';
 import { NextFunction, Request, Response } from 'express';
+import { z } from 'zod';
 
 class ProductsController {
   async index(req: Request, res: Response, next: NextFunction) {
     try {
       return res.json({ message: 'OK' });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0, 'Price must be greater than 0'),
+      });
+
+      const { name, price } = bodySchema.parse(req.body);
+
+      return res.status(201).json({ name, price });
     } catch (error) {
       next(error);
     }
