ad37a56d4150afbcb3861594111b5e77acd28042
miguelalchaar <alchaarcs@gmail.com>
2026-01-13 14:03:15 -0300

    feat: implement create session logic in TablesSessionsController and add TableSessionsRepository type

--- src/controllers/tables-sessions-controller.ts
+++ src/controllers/tables-sessions-controller.ts
@@ -1,9 +1,31 @@
-import exp from 'constants';
 import { Request, Response, NextFunction } from 'express';
+import { AppError } from '@/utils/AppError';
+import z from 'zod';
+import { knex } from '../database/knex';
 
 class TablesSessionsController {
   async create(req: Request, res: Response, next: NextFunction) {
     try {
+      const bodySchema = z.object({
+        table_id: z.number(),
+      });
+
+      const { table_id } = bodySchema.parse(req.body);
+
+      const session = await knex<TableSessionsRepository>('table_sessions')
+        .where({ table_id })
+        .orderBy('opened_at', 'desc')
+        .first();
+
+      if (session && !session.closed_at) {
+        throw new AppError('This table is already in use.');
+      }
+
+      await knex<TableSessionsRepository>('table_sessions').insert({
+        table_id,
+        opened_at: knex.fn.now(),
+      });
+
       return res.status(201).json();
     } catch (error) {
       next(error);
