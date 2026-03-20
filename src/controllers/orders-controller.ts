227156f6cf6e7875355a72e1a0819f45cee61782
miguelalchaar <alchaarcs@gmail.com>
2026-03-20 12:03:13 -0300

    feat: add order creation logic and table sessions index route

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
-import { AppError } from '@/utils/AppError'; 
+import { AppError } from '@/utils/AppError';
 import { knex } from '../database/knex';
 import { z } from 'zod';
 
-export class OrdersController {
+class OrdersController {
   async create(req: Request, res: Response, next: NextFunction) {
     try {
       const bodySchema = z.object({
@@ -20,25 +20,44 @@ export class OrdersController {
         .where({ id: table_session_id })
         .first();
 
-        if (!session) {
-          throw new AppError('Table session not found', 404);
-        }
+      if (!session) {
+        throw new AppError('Table session not found', 404);
+      }
 
-        if (session.closed_at) {
-          throw new AppError('Table session is already closed', 400);
-        }
+      if (session.closed_at) {
+        throw new AppError('Table session is already closed', 400);
+      }
 
-        const product = await knex<ProductRepository>('products')
+      const product = await knex<ProductRepository>('products')
         .where({ id: product_id })
         .first();
 
-        if (!product) {
-          throw new AppError('Product not found', 404);
-        }
+      if (!product) {
+        throw new AppError('Product not found', 404);
+      }
 
-      return res.status(201).json(session);
+      await knex<OrderRepository>('orders').insert({
+        table_session_id,
+        product_id,
+        quantity,
+        price: product.price,
+      });
+
+      return res.status(201).json();
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async index(req: Request, res: Response, next: NextFunction)  {
+    try {
+      return res.json()
     } catch (error) {
       next(error);
     }
   }
 }
+
+
+
+export { OrdersController };
